33a812ccef1daf7ea4d2fddfdb6e46f4b2473f2e
Harrison Barclay <barclay.h@husky.neu.edu>
2016-07-26 00:56:57 -0400

    basic structure planning

--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -1,8 +1,36 @@
 
+#ifndef SRC_NEURALNETWORK
+#define SRC_NEURALNETWORK
 
+#include<vector>
 
 class NeuralNetwork
 {
 
 
+public:
+	int numLayers;
+	const int numInputNeurons;
+	const int numOutputNeurons;
+	const int numLayer1Neurons;
+	const int numLayer2Neurons;
+	
+	struct Neuron
+	{
+		double bias = 0;
+	}
+
+	struct Connection
+	{
+		int from;
+		int to;
+		
+		double weight;
+	}
+
+	std::vector<std::unique_ptr<Neuron>> neurons;
+	std::vector<std::unique_ptr<Connection>> connections;	
 };
+
+
+#endif
