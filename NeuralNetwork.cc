40bd2e20ccd5ade98030303eac9087ed194d088b
Harrison Barclay <barclay.h@husky.neu.edu>
2016-07-26 22:36:44 -0400

    network initialization function

--- NeuralNetwork.cc
+++ NeuralNetwork.cc
@@ -1,5 +1,81 @@
+#include "NeuralNetwork.h"
+#include <iostream>
+#include <cstdlib>
 
 
-NeurelNetwork::NeuralNetwork(){
-		
+
+
+double NeuralNetwork::randomWeight()
+{
+	return ((double) rand() / (RAND_MAX)) * 2.0 - 1;
+}
+
+
+void Initialize()
+{
+	//construct vector of neurons
+	for(int i = 0; i < totalNeurons; i++ )
+	{
+		auto neuron = std::unique_ptr<Neuron>(new Neuron());
+		neuron->bias = randomBias();
+		neurons.push_back(std::move(neuron));
+	}
+
+	//construct vector of connections
+	for(int i = 0; i < numInputNeurons; i++)
+	{
+		for(int j = 0; j < numLayer1Neurons; j++)
+		{
+			auto connection = std::unique_ptr<Connection>(new Connection());
+			connection->from = i;
+			connection->to = numInputNeurons + j;
+			connection->weight = randomWeight();
+			connections.push_back(std::move(connection));
+		}
+	}
+
+
+	for(int i = 0; i < numLayer1Neurons; i++)
+	{
+		for(int j = 0; j < numLayer2Neurons; j++)
+		{
+			auto connection = std::unique_ptr<Connection>(new Connection());
+			connection->from = numInputNeurons + i;
+			connection->to = numInputNeurons + numLayer1Neurons + j;
+			connection->weight = randomWeight();
+			connections.push_back(std::move(connection));
+		}
+	}
+
+	for(int i = 0; i < numLayet2Neurons; i++)
+	{
+		for(int j = 0; j < numOutputNeurons; j++)
+		{
+			auto connection = std::unique_ptr<Connection>(new Connection());
+			connection->from = numInputNeurons + numLayer1Nerons + i;
+			connection->to = numInputNeurons + numLayer1Neurons + numLayer2Neurons + j;
+			connection->weight = randomWeight();
+			connections.push_back(std::move(connection));	
+		}
+	}	
+}
+
+double NeuralNetwork::getOutput(int neuron)
+{
+	
+}
+
+void NeuralNetwork::setNeuronOutput(int neuron, double scale)
+{
+
+}
+
+void NeuralNetwork::feedForward()
+{
+
+}
+
+double NeuralNetwork::sigmoid(double x)
+{
+	return 1 / (1 + exp(-x));
 }
--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -6,18 +6,26 @@
 
 class NeuralNetwork
 {
-
-
+	double sigmoid(double x);
+	double randomWeight();
+	double randomBias();
 public:
 	int numLayers;
 	const int numInputNeurons;
 	const int numOutputNeurons;
 	const int numLayer1Neurons;
 	const int numLayer2Neurons;
+	const int totalNeurons = numInputNeurons + numOutPutNeurons
+		+ numLayer1Neurons + numLayer2Neurons;
+	const int totalConnections = 
+		numInputNeurons * numLayer1Nuerons +
+		numLayer1Neurons * numLayer2Neurons +
+		numLayer2Neurons * numOutputNeurons;
 	
 	struct Neuron
 	{
 		double bias = 0;
+		double output = 0;
 	}
 
 	struct Connection
@@ -30,6 +38,13 @@ public:
 
 	std::vector<std::unique_ptr<Neuron>> neurons;
 	std::vector<std::unique_ptr<Connection>> connections;	
+
+	void Initialize();
+	double getOutput(int neuron);
+	void setNeuronOutput(int neuron, double scale);
+	void feedForward();
+	std::unique_ptr<NeuralNetwork> Clone();
+
 };
 
 
