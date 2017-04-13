60048ae2eb3dbf84467e535cec570e676badc261
Harrison Barclay <barclay.h@husky.neu.edu>
2017-04-13 12:45:22 -0400

    OpenBlas build directory is hardware specific

--- OpenBLAS-build/lib/cmake/openblas/OpenBLASConfig.cmake
+++
@@ -1,3 +0,0 @@
-SET(OpenBLAS_VERSION "0.2.20.dev")
-SET(OpenBLAS_INCLUDE_DIRS /home/user/git-repos/NeuralNetworkCPU/OpenBLAS-build/include)
-SET(OpenBLAS_LIBRARIES /home/user/git-repos/NeuralNetworkCPU/OpenBLAS-build/lib/libopenblas.so)
