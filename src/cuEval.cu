883c254284e0f9b1cb09624794dc9ad7da8ab76d
Harrison Barclay <barclay.h@husky.neu.edu>
2017-04-18 21:52:27 -0400

    comments

--- src/cuEval.cu
+++ src/cuEval.cu
@@ -1,16 +1,20 @@
 
 __global__ void matrixMul()
 {
-
+	// optimized matrix multiplication
 }
 
 __global__ void launchEvals()
 {
-
+	// call matrixMul three times (different streams for each eval)
 }
 
 void launchEvals()
 {
+
+	// copy all data to global memory
+
+	// call launch Evals
 
 
 }
